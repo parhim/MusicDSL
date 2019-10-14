0f60029fa9ef47988b79b5aa2025a9ac03db305a
annnnnieee <anastasiya.vt.s@gmail.com>
2019-10-14 11:29:09 -0700

    removed unused files

--- NULL/src/main.ts
+++ NULL/src/main.ts
@@ -1,6 +1,6 @@
 import {MusicProgram} from "./dsl/MusicProgram";
 
-let musicProgram = new MusicProgram("valid/sample.tdot");
+let musicProgram = new MusicProgram("grammarsamples/grammar.txt");
 try{
     musicProgram.parse();
     musicProgram.compile();
